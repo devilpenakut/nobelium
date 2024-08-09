a32e88e58f295f5f90d92497ae97a80fb01bc1e3
devilpenakut <devilpenakut@gmail.com>
2024-08-09 11:43:02 +0700

    Update DarkModeToggle.js

--- components/DarkModeToggle.js
+++ components/DarkModeToggle.js
@@ -4,9 +4,10 @@ const DarkModeToggle = () => {
   const { dark, toggleDark } = useTheme()
 
   return (
-    <button
-      className="bg-gray-200 dark:bg-gray-800 rounded-full p-2"
+   <button
+      className="dark-mode-toggle bg-gray-200 dark:bg-gray-800 rounded-full p-2"
       onClick={(e) => {
+        e.stopPropagation(); // Pastikan event propagation dihentikan jika perlu
         console.log("Toggle clicked!"); // Tambahkan log ini
         toggleDark();
       }}
